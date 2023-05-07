2f16f4d71d2d61b458976267bfd6ec9852c49589
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2023-05-07 16:42:03 -0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <h4>Esto es un foking footer</h4>
+      <span>@bruumendiburu on socials</span>
+      <h5>Shopping Carte con useContext & useReducer</h5>
+    </footer>
+  )
+}
